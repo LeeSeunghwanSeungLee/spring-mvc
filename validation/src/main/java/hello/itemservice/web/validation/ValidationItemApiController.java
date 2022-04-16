f8e589831678a252e0b39cae5c59be60d31c7b86
lshn1007 <lshn1007@hanyang.ac.kr>
2022-04-16 19:46:07 +0900

    feat: add validation lombok test controller

---
+++ validation/src/main/java/hello/itemservice/web/validation/ValidationItemApiController.java
@@ -0,0 +1,28 @@
+package hello.itemservice.web.validation;
+
+import hello.itemservice.web.validation.form.ItemSaveForm;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/validation/api/items")
+public class ValidationItemApiController {
+
+    @PostMapping("/add")
+    public Object addItem(@RequestBody @Validated ItemSaveForm form, BindingResult bindingResult) {
+        log.info("아이템 저장 호출");
+
+        if (bindingResult.hasErrors()) {
+            log.warn("검증 오류 발생 {}", bindingResult);
+            return bindingResult.getAllErrors();
+        }
+        log.info("아이템 저장 성공");
+        return form;
+    }
+}
