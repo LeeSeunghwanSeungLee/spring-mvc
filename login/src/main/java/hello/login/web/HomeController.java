23cf2fc2d9d48086889faaefa660b53db3d2aae3
lshn1007 <lshn1007@hanyang.ac.kr>
2022-04-27 19:35:33 +0900

    feat: add Session
    self made session manager && HttpSession Controller

--- login/src/main/java/hello/login/web/HomeController.java
+++ login/src/main/java/hello/login/web/HomeController.java
@@ -1,15 +1,43 @@
 package hello.login.web;
 
+import hello.login.domain.member.Member;
+import hello.login.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
 
-    @GetMapping("/")
+    private final MemberRepository memberRepository;
+
+//    @GetMapping("/")
     public String home() {
-        return "redirect:/items";
+        return "home";
+    }
+
+//    @GetMapping("/")
+    public String homeLogin(@CookieValue(name = "memberId", required = false) Long memberId, Model model) {
+        if (memberId == null)
+            return "home";
+
+        // login
+        Member loginMember = memberRepository.findById(memberId);
+        if (loginMember == null)
+            return "home";
+
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+    }
+
+    public String homeLoginV2(HttpServletRequest reuqest, Model, model) {
+        // 세션 관리자에 의해 저장된 회원 정보 조회
     }
 }
